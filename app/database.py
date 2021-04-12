73eb435433227f0207be19c718daad3c7c3fba8d
Rohit Narayanan <rohitn2@illinois.edu>
2021-04-12 00:27:47 +0000

    Pushed python scripts

--- app/database.py
+++ app/database.py
@@ -90,3 +90,272 @@ def remove_gym_by_id(GymID: int) -> None:
     query = 'Delete From Gyms where GymID={};'.format(GymID)
     conn.execute(query)
     conn.close()
+
+
+#Physical Data:
+'''
+CREATE TABLE PhysicalData (
+    Username VARCHAR(50), 
+    LastMuscleGroup VARCHAR(50), 
+    Injury VARCHAR(50), 
+    LastRecorded VARCHAR(50), 
+    WorkSplit VARCHAR(50),
+    PRIMARY KEY(Username)
+);
+'''
+#Insert Row
+def insertNewPhysicalData(Username: str, LastMuscleGroup: str, Injury: str, LastRecorded: str, WorkSplit: str):
+    conn = db.connect()
+    query = 'Insert Into PhysicalData (Username, LastMuscleGroup, Injury, LastRecorded, WorkSplit) VALUES ("{}", "{}", "{}", "{}", "{}");'.format(Username, LastMuscleGroup, Injury, LastRecorded, WorkSplit)
+    conn.execute(query)
+    conn.close()
+
+#Search by username
+def fetchPhysicalData(Username: str) -> dict:
+    conn = db.connect()
+    statement = 'SELECT Username, LastMuscleGroup, Injury, LastRecorded, WorkSplit FROM PhysicalData WHERE Username LIKE "%{0}%" ;'.format(Username)
+    query = text(statement)
+    print(query)
+    query_results = conn.execute(query).fetchall() #Check to make sure this returns correctly
+    #print(query_results[0])
+    physicalData_list = []
+    for result in query_results:
+        item = {
+            "Username": result[0],
+            "LastMuscleGroup": result[1],
+            "Injury": result[2],
+            "LastRecorded": result[3],
+            "WorkSplit": result[4]
+        }
+        physicalData_list.append(item)
+    conn.close()
+    return physicalData_list
+
+#update by username
+def updatePhysicalDataLastMuscleGroup(Username: int, text: str) -> None:
+    conn = db.connect()
+    query = 'Update PhysicalData set LastMuscleGroup = "{}" where Username = "{}";'.format(text, Username)
+    conn.execute(query)
+    conn.close()
+
+def updatePhysicalDataInjury(Username: int, text: str) -> None:
+    conn = db.connect()
+    query = 'Update PhysicalData set Injury = "{}" where Username = "{}";'.format(text, Username)
+    conn.execute(query)
+    conn.close()
+
+def updatePhysicalDataLastRecorded(Username: int, text: str) -> None:
+    conn = db.connect()
+    query = 'Update PhysicalData set LastRecorded = "{}" where Username = "{}";'.format(text, Username)
+    conn.execute(query)
+    conn.close()
+
+def updatePhysicalDataWorkSplit(Username: int, text: str) -> None:
+    conn = db.connect()
+    query = 'Update PhysicalData set WorkSplit = "{}" where Username = "{}";'.format(text, Username)
+    conn.execute(query)
+    conn.close()
+
+
+#Delete by username
+def removePhysicalDataByUsername(Username: str) -> None:
+    """ remove entries based on task ID """
+    conn = db.connect()
+    query = 'Delete From PhysicalData where Username="{}";'.format(Username)
+    conn.execute(query)
+    conn.close()
+
+#Advanced Query
+def getAvailibleReservations(Username: str) -> dict:
+    conn = db.connect()
+    print(Username)
+    if len(Username) > 0:
+        statement = 'SELECT ReservationID, GymName, StartTime, EndTime, Day, Month, Year \
+            FROM Reservations r JOIN Gyms g USING (GymID) WHERE GymID IN \
+            (SELECT GymID FROM Users u JOIN Gyms g USING(University) WHERE Username = "{}") \
+            AND r.Username IS NULL ORDER BY Day DESC, StartTime DESC;'.format(Username)
+    query = text(statement)
+    print(query)
+    query_results = conn.execute(query).fetchall()
+    conn.close()
+    reservations = []
+    for result in query_results:
+        item = {
+            "ReservationID": result[0],
+            "GymName": result[1],
+            "StartTime": result[2],
+            "EndTime": result[3],
+            "Day": result[4],
+            "Month": result[5],
+            "Year": result[6],
+        }
+        reservations.append(item)
+    
+    return reservations
+
+
+#Progress:
+
+def fetch_progress(exercise: str) -> dict:
+    conn = db.connect()
+    if len(exercise) > 0:
+        statement = 'SELECT ProgressID, Exercise, Set_Size, Exercise_Stat FROM Progress WHERE Exercise LIKE "%{0}%" ORDER BY Exercise ASC;'.format(exercise)
+    else:
+        statement = 'SELECT ProgressID, Exercise, Set_Size, Exercise_Stat FROM Progress ORDER BY Exercise ASC;'
+    query = text(statement)
+    print(query)
+    query_results = conn.execute(query).fetchall()
+    conn.close()
+    progress_list = []
+    for result in query_results:
+        item = {
+            "ProgressID": result[0],
+            "Exercise": result[1],
+            "Set_Size": result[2],
+            "Exercise_Stat": result[3]
+        }
+        progress_list.append(item)
+    return progress_list
+
+
+def fetch_progmax(exercise: str) -> dict:
+    conn = db.connect()
+    if len(exercise) > 0:
+        statement = 'SELECT Exercise, MAX(Set_Size) From Users Natural Join Progress WHERE Exercise LIKE "%{0}%" GROUP BY Exercise'.format(exercise)
+    query = text(statement)
+    print(query)
+    query_results = conn.execute(query).fetchall()
+    conn.close()
+    progmax_list = []
+    for result in query_results:
+        item = {
+            "Exercise": result[0],
+            "Max_Set_Size": result[1]
+        }
+        progmax_list.append(item)
+    return progmax_list
+
+def update_prog_exer(exercise: str, ProgressID:int) -> None:
+    conn = db.connect()
+    query = 'Update Progress set Exercise = "{0}" where ProgressID = {1};'.format(exercise, ProgressID)
+    conn.execute(query)
+    conn.close()
+def update_prog_set(set_size: int, ProgressID:int) -> None:
+    conn = db.connect()
+    query = 'Update Progress set Set_Size = {0} where ProgressID = {1};'.format(set_size, ProgressID)
+    conn.execute(query)
+    conn.close()
+
+def update_prog_stat(exercise_stat: int, ProgressID:int) -> None:
+    conn = db.connect()
+    query = 'Update Progress set Exercise_Stat = {0} where ProgressID = {1};'.format(exercise_stat, ProgressID)
+    conn.execute(query)
+    conn.close()
+
+def insert_new_progress(ProgressID: int, Exercise: str, Set_Size: int, Exercise_Stat: int):
+    conn = db.connect()
+    query = 'Insert Into Progress (ProgressID, Exercise, Set_Size, Exercise_Stat) VALUES ("{}", "{}", "{}", "{}");'.format(ProgressID, Exercise, Set_Size, Exercise_Stat)
+    conn.execute(query)
+    conn.close()
+
+def remove_progress_by_id(ProgressID: int) -> None:
+    """ remove entries based on task ID """
+    conn = db.connect()
+    query = 'Delete From Progress where ProgressID={};'.format(ProgressID)
+    conn.execute(query)
+    conn.close()
+
+# Users 
+
+def update_user_name(email: str, text: str) -> None:
+    conn = db.connect()
+    query = 'Update Users set Username = "{1}" where Email = "{0}";'.format(text, email)
+    conn.execute(query)
+    conn.close()
+
+def update_user_uni(email: str, text: str) -> None:
+    conn = db.connect()
+    query = 'Update Users set University = "{1}" where Email = "{0}";'.format(text, email)
+    conn.execute(query)
+    conn.close()
+
+def update_user_fname(email: str, text: str) -> None:
+    conn = db.connect()
+    query = 'Update Users set Firstname = "{1}" where Email = "{0}";'.format(text, email)
+    conn.execute(query)
+    conn.close()
+
+def update_user_lname(email: str, text: str) -> None:
+    conn = db.connect()
+    query = 'Update Users set Lastname = "{1}" where Email = "{0}";'.format(text, email)
+    conn.execute(query)
+    conn.close()
+
+def update_user_password(email: str, text: str) -> None:
+    conn = db.connect()
+    query = 'Update Users set Password = "{1}" where Email = "{0}";'.format(text, email)
+    conn.execute(query)
+    conn.close()
+
+def update_user_covidstatus(email: str, text: str) -> None:
+    conn = db.connect()
+    query = 'Update Users set CovidStatus = "{1}" where Email = "{0}";'.format(text, email)
+    conn.execute(query)
+    conn.close()
+
+def insert_new_user(FirstName: str, LastName: str, Email: str, University: str, Username: str, Password: str, CovidStatus: str):
+    conn = db.connect()
+    query = 'Insert Into Users (FirstName, LastName, Email, University, Username, Password, CovidStatus) VALUES ("{}", "{}", "{}", "{}", "{}", "{}", "{}");'.format(FirstName, LastName, Email, University, Username, Password, CovidStatus)
+    conn.execute(query)
+    conn.close()
+
+def remove_user_by_email(Email: str) -> None:
+    conn = db.connect()
+    query = 'Delete From Users where Email="{}";'.format(Email)
+    conn.execute(query)
+    conn.close()
+
+def fetch_users(username: str, university: str) -> dict:
+    conn = db.connect()
+    if len(username) > 0 and len(university) > 0:
+        statement = 'SELECT Email, University, Username, CovidStatus FROM Users WHERE Username LIKE "%{0}%" AND University LIKE "%{1}%" ORDER BY Username DESC, University ASC;'.format(username, university)
+    elif len(username) > 0:
+        statement = 'SELECT Email, University, Username, CovidStatus FROM Users WHERE Username LIKE "%{0}%" ORDER BY Username DESC, University ASC;'.format(username)
+    elif len(university) > 0:
+        statement = 'SELECT Email, University, Username, CovidStatus FROM Users WHERE University LIKE "%{0}%" ORDER BY University ASC, Username DESC;'.format(university)
+    else:
+        statement = 'SELECT Email, University, Username, CovidStatus FROM Users ORDER BY Username DESC, University ASC;'
+    query = text(statement)
+    print(query)
+    query_results = conn.execute(query).fetchall()
+    conn.close()
+    users_list = []
+    for result in query_results:
+        item = {
+            "Email": result[0],
+            "University": result[1],
+            "Username": result[2],
+            "Covid Status": result[3]
+        }
+        users_list.append(item)
+    return users_list
+	
+def fetch_lift_records(exercise: str) -> dict:
+    conn = db.connect()
+    if len(exercise) > 0:
+        statement = 'CREATE VIEW userMaxes as SELECT DISTINCT pr.Username, u.University, pr.Exercise, MAX(tempUser.Exercise_Stat) as maxUser FROM Progress pr NATURAL JOIN Users u, (SELECT p.Username, p.Exercise_Stat FROM Progress p WHERE p.Exercise = "{0}") as tempUser WHERE pr.Exercise = "{0}" and pr.Username = tempUser.Username GROUP BY pr.Username, u.University;\n'.format(exercise) 
+        str2 = 'SELECT DISTINCT u.University, um.Exercise, MAX(um.maxUser) as maxExercise FROM userMaxes um LEFT JOIN Users u USING(Username) GROUP BY u.University, um.Exercise ORDER BY maxExercise DESC LIMIT 15\n'
+        str3 = 'DROP VIEW IF EXISTS userMaxes'
+
+    conn.execute(statement+str2+str3)
+    query_results = conn.execute(query).fetchall()
+    conn.close()
+    lift_list = []
+    for result in query_results:
+        item = {
+            "University": result[0],
+            "Exercise": result[1],
+            "Record": result[2]
+        }
+        lift_list.append(item)
+    return lift_list
--- app/routes.py
+++ app/routes.py
@@ -89,3 +89,240 @@ def removegympage():
         db_helper.remove_gym_by_id(gymid)
         return render_template("removegym.html")
     return render_template("removegym.html")
+
+#Physical Data
+@app.route("/PhysicalData", methods=['GET', 'POST'])
+def PhysicalDataPage():
+    print(request.method)
+    # Check if it is a post req
+    if request.method == 'POST':
+        # collect gymname and university information from search bar(s) in gyms.html
+        Username = request.form.get("Username")
+        print(Username)
+        # call fetch_gyms with passed parameters and get query return list.
+        items = db_helper.fetchPhysicalData(Username)
+        print("test")
+        # return rendered template of gyms.html with list as items
+        return render_template("PhysicalData.html", items=items)
+    else:
+        #If not POST req occurs, display all gyms to user
+        items = db_helper.fetchPhysicalData("")
+        return render_template("PhysicalData.html", items=items)
+    return render_template("PhysicalData.html")
+
+@app.route("/findReservations", methods=['GET', 'POST'])
+def reservationsPage():
+    print(request.method)
+    if request.method == 'POST':
+        username = request.form.get("Username")
+        items = db_helper.getAvailibleReservations(username)
+        print("test")
+        return render_template("findReservations.html", items=items)
+    return render_template("findReservations.html")
+
+@app.route("/addPhysicalData", methods=['GET', 'POST'])
+def addPhysicalDataPage():
+    #Username, LastMuscleGroup, Injury, LastRecorded, WorkSplit
+    print(request.method)
+    if request.method == 'POST':
+        Username = request.form.get("Username")
+        print(Username)
+        lastMuscleGroup= request.form.get("LastMuscleGroup")
+        print(lastMuscleGroup)
+        injury = request.form.get("Injury")
+        print(injury)
+        lastRecorded = request.form.get("LastRecorded")
+        print(lastRecorded)
+        workSplit = request.form.get("WorkSplit")
+        print(workSplit)
+        db_helper.insertNewPhysicalData(Username, lastMuscleGroup, injury, lastRecorded, workSplit)
+        return render_template("addPhysicalData.html")
+    return render_template("addPhysicalData.html")
+
+@app.route("/updatePhysicalData", methods=['GET', 'POST'])
+def updatePDpage():
+    print(request.method)
+    if request.method == 'POST':
+        Username = request.form.get("Username")
+        print(Username)
+        lastMuscleGroup = request.form.get("LastMuscleGroup")
+        injury = request.form.get("Injury")
+        lastRecorded = request.form.get("LastRecorded")
+        workSplit = request.form.get("WorkSplit")
+        if len(lastMuscleGroup) > 0:
+            db_helper.updatePhysicalDataLastMuscleGroup(Username, lastMuscleGroup)
+        if len(injury) > 0:
+            db_helper.updatePhysicalDataInjury(Username, injury)
+        if len(lastRecorded) > 0:
+            db_helper.updatePhysicalDataLastRecorded(Username, lastRecorded)
+        if len(workSplit) > 0:
+            db_helper.updatePhysicalDataWorkSplit(Username, workSplit)
+        return render_template("updatePhysicalData.html")
+    return render_template("updatePhysicalData.html")
+
+@app.route("/removePhysicalData", methods=['GET', 'POST'])
+def removePDpage():
+    print(request.method)
+    if request.method == 'POST':
+        Username = request.form.get("Username")
+        print(Username)
+        db_helper.removePhysicalDataByUsername(Username)
+        return render_template("removePhysicalData.html")
+    return render_template("removePhysicalData.html")
+
+    # PROGRESS
+
+@app.route("/progress", methods=['GET', 'POST'])
+def progresspage():
+    print(request.method)
+    # Check if it is a post req
+    if request.method == 'POST':
+        # collect gymname and university information from search bar(s) in gyms.html
+        exercise = request.form.get("exercise")
+        print(exercise)
+        # call fetch_gyms with passed parameters and get query return list.
+        items = db_helper.fetch_progress(exercise)
+        print("test")
+        # return rendered template of gyms.html with list as items
+        return render_template("progress.html", items=items)
+    else:
+        #If not POST req occurs, display all gyms to user
+        items = db_helper.fetch_progress("")
+        return render_template("progress.html", items=items)
+    return render_template("progress.html")
+
+@app.route("/findmaxprogress", methods=['GET', 'POST'])
+def maxpage():
+    print(request.method)
+    if request.method == 'POST':
+        exercise = request.form.get("exercise")
+        items = db_helper.fetch_progmax(exercise)
+        print("test")
+        return render_template("findmaxprogress.html", items=items)
+    return render_template("findmaxprogress.html")
+
+@app.route("/addprogress", methods=['GET', 'POST'])
+def addprogpage():
+    print(request.method)
+    if request.method == 'POST':
+        progressid = request.form.get("progressid")
+        print(progressid)
+        exercise = request.form.get("exercise")
+        print(exercise)
+        set_size = request.form.get("set_size")
+        print(set_size)
+        exercise_stat = request.form.get("exercise_stat")
+        print(exercise_stat)
+        db_helper.insert_new_progress(progressid, exercise, set_size, exercise_stat)
+        return render_template("addprogress.html")
+    return render_template("addprogress.html")
+
+@app.route("/updateprogress", methods=['GET', 'POST'])
+def updateprogpage():
+    print(request.method)
+    if request.method == 'POST':
+        progressid = request.form.get("progressid")
+        print(progressid)
+        exercise = request.form.get("exercise")
+        set_size = request.form.get("set_size")
+        exercise_stat = request.form.get("exercise_stat")
+        if len(exercise) > 0:
+            db_helper.update_prog_exer(exercise, progressid)
+        if len(set_size) > 0:
+            db_helper.update_prog_set(set_size, progressid)
+        if len(exercise_stat) > 0:
+            db_helper.update_prog_stat(exercise_stat, progressid)
+        return render_template("updateprogress.html")
+    return render_template("updateprogress.html")
+
+@app.route("/removeprogress", methods=['GET', 'POST'])
+def removeprogpage():
+    print(request.method)
+    if request.method == 'POST':
+        progressid = request.form.get("progressid")
+        print(progressid)
+        db_helper.remove_progress_by_id(progressid)
+        return render_template("removeprogress.html")
+    return render_template("removeprogress.html")
+
+
+#USERS
+
+@app.route("/users", methods=['GET', 'POST'])
+def userpage():
+    print(request.method)
+    # Check if it is a post req
+    if request.method == 'POST':
+        # collect gymname and university information from search bar(s) in users.html
+        username = request.form.get("Username")
+        print(username)
+        university = request.form.get("University")
+        print(university)
+        # call fetch_users with passed parameters and get query return list.
+        items = db_helper.fetch_users(username, university)
+        print("test")
+        # return rendered template of gyms.html with list as items
+        return render_template("users.html", items=items)
+    else:
+        #If not POST req occurs, display all gyms to user
+        items = db_helper.fetch_users("", "")
+        return render_template("users.html", items=items)
+    return render_template("users.html")
+
+
+@app.route("/addusers", methods=['GET', 'POST'])
+def adduserpage():
+    print(request.method)
+    if request.method == 'POST':
+        firstname = request.form.get("firstname")
+        print(firstname)
+        lastname = request.form.get("lastname")
+        print(lastname)
+        email = request.form.get("email")
+        university = request.form.get("university")
+
+        username = request.form.get("username")
+        password = request.form.get("password")
+        print(password)
+        covidstatus = request.form.get("covidstatus")
+        print(covidstatus)
+        db_helper.insert_new_user(firstname, lastname, email, university, username, password, covidstatus)
+        return render_template("addusers.html")
+    return render_template("addusers.html")
+
+@app.route("/updateusers", methods=['GET', 'POST'])
+def updateuserpage():
+    print(request.method)
+    if request.method == 'POST':
+        email = request.form.get("email")
+        print(email)
+        firstname = request.form.get("firstname")
+        lastname = request.form.get("lastname")
+        university = request.form.get("university")
+        username = request.form.get("username")
+        password = request.form.get("password")
+        covidstatus = request.form.get("covidstatus")
+        if len(firstname) > 0:
+            db_helper.update_user_fname(firstname, email)
+        if len(lastname) > 0:
+            db_helper.update_user_lname(lastname, email)
+        if len(university) > 0:
+            db_helper.update_user_uni(university, email)
+        if len(username) > 0:
+            db_helper.update_user_name(username, email)
+        if len(password) > 0:
+            db_helper.update_user_password(password, email)
+        if len(covidstatus) > 0:
+            db_helper.update_user_covidstatus(covidstatus, email)
+        return render_template("updateusers.html")
+    return render_template("updateusers.html")
+
+@app.route("/deleteuser", methods=['GET', 'POST'])
+def removeuserpage():
+    print(request.method)
+    if request.method == 'POST':
+        email = request.form.get("email")
+        print(email)
+        db_helper.remove_user_by_email(email)
+        return render_template("deleteuser.html")
+    return render_template("deleteuser.html")
